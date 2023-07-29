9a8749cf6e7b0cdce2acc8d5eb37578ecb716043
Hemanth Kumar <avisihks@gmail.com>
2023-07-29 22:16:54 +0530

    Log requests

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const http = require("http");
 
-// Create a web server using the createServer function
 const server = http.createServer((request, response) => {
-  // The function inside createServer is called for every incoming HTTP request
-
-  // Set the HTTP response header
-  response.writeHead(200, { "Content-Type": "text/plain" });
-
-  // Write the response content
-  response.end("Avisi Hemanth Kumar");
+  console.log({
+    url: request.url,
+    method: request.method,
+    headers: request.headers,
+  });
 });
 
-// Start the server and listen on a specific port
 const PORT = 4000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+server.listen(PORT);
