38d15e40198de3705e473e8dd72095132194b346
Hemanth Kumar <avisihks@gmail.com>
2023-07-29 23:06:09 +0530

    Show response based on routing

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const server = http.createServer((request, response) => {
     method: request.method,
     headers: request.headers,
   });
+  switch (request.url) {
+    case "/home":
+      response.setHeader("Content-Type", "text/html");
+      response.write("Welcome home");
+      response.end();
+      break;
+    case "/about":
+      response.setHeader("Content-Type", "text/html");
+      response.write("Welcome to About Us page");
+      response.end();
+      break;
+    case "/node":
+      response.setHeader("Content-Type", "text/html");
+      response.write("Welcome to my Node Js project");
+      response.end();
+      break;
+    default:
+      response.setHeader("Content-Type", "text/html");
+      response.write("Default Page");
+      response.end();
+      break;
+  }
 });
 
 const PORT = 4000;
